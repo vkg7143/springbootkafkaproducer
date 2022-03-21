d79d92d170eea9c143bafb06451c861751dc9963
vkg7143 <74658870+vkg7143@users.noreply.github.com>
2022-03-21 18:44:14 +0530

    project setup commit

---
+++ src/main/java/com/vcs/kafkaproducer/KafkaproducerApplication.java
@@ -0,0 +1,13 @@
+package com.vcs.kafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaproducerApplication.class, args);
+	}
+
+}
