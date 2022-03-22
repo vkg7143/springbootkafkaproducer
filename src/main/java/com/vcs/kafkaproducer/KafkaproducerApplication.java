f631f601d4b95f299ad2491474928d15fbd16b43
vivekkumar <vkg7143@gmail.com>
2022-03-22 17:49:03 +0530

    adding rest

--- src/main/java/com/vcs/kafkaproducer/KafkaproducerApplication.java
+++ src/main/java/com/vcs/kafkaproducer/KafkaproducerApplication.java
@@ -1,13 +1,18 @@
 package com.vcs.kafkaproducer;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.vcs.kafkaproducer.service.StudentService;
 
 @SpringBootApplication
 public class KafkaproducerApplication {
+	StudentService studentService = new StudentService();
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(KafkaproducerApplication.class, args);
+
 	}
 
 }
